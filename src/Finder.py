c26c858488b08a04ca4a33fc17a0b7d79ea75f99
Benjamin O'Sullivan <benjamin.osullivan@d-fine.com>
2024-03-27 16:18:13 +0100

    more cleaning

--- src/Finder.py
+++ src/Finder.py
@@ -33,5 +33,5 @@ class DataFinder():
         return vbaDirectories
 
     def findRulesFiles(self) -> list:
-        rulesFiles = [os.path.join(root,file) for root,dirs,files in os.walk('source/testFilesAndDirs/') for file in files if file.startswith("Rules_")]
+        rulesFiles = [os.path.join(root,file) for root,dirs,files in os.walk('source/') for file in files if file.startswith("Rules_")]
         return rulesFiles
