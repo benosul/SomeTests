348b8d021df49558a5df54afae330daf1bd65fc9
benosul <148952119+benosul@users.noreply.github.com>
2024-04-26 10:17:02 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Access the arguments as environment variables
+echo "Arg1: $ARG1"
+echo "Arg2: $ARG2"
+echo "Arg3: $ARG3"
+
+# Execute your Python script or desired behavior
+python app.py "$ARG1" "$ARG2" "$ARG3"
