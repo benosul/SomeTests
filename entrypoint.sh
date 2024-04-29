82a38ef103acf8b56c644c3868693b284cf6cd15
benosul <148952119+benosul@users.noreply.github.com>
2024-04-29 15:09:24 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
 # Access the arguments as environment variables
-echo "Arg1: $ARG1"
-echo "Arg2: $ARG2"
-echo "Arg3: $ARG3"
+echo "Hello World"
 
-# Execute your Python script or desired behavior
-python app.py "$ARG1" "$ARG2" "$ARG3"
